ca3d5f4e830897e555bd9c53a305ef556c2f437c
Aadi Poddar <aadipoddarmail@gmail.com>
2022-10-27 16:48:26 +0530

    Add and Move compiler APIs into a library project
    The Compilaton API will serve as our public API for the compiler. The compilation
    holds onto the state of the entire program, specifically the syntax
    tree and eventually also the declared symbols.

---
+++ Minsk/CodeAnalysis/Compilation.cs
@@ -0,0 +1,30 @@
+﻿using Minsk.CodeAnalysis.Binding;
+using Minsk.CodeAnalysis.Syntax;
+
+namespace Minsk.CodeAnalysis
+{
+    public sealed class Compilation
+    {
+        public Compilation(SyntaxTree syntax)
+        {
+            Syntax = syntax;
+        }
+
+        public SyntaxTree Syntax { get; }
+
+        public EvaluationResult Evaluate()
+        {
+            var binder = new Binder();
+            var boundExpression = binder.BindExpression(Syntax.Root);
+
+            var diagnostics = Syntax.Diagnostics.Concat(binder.Diagnostics).ToArray();
+            if (diagnostics.Any())
+                return new EvaluationResult(diagnostics, null);
+
+            var evaluator = new Evaluator(boundExpression);
+            var value = evaluator.Evaluate();
+
+            return new EvaluationResult(Array.Empty<string>(), value);
+        }
+    }
+}
--- mc/Program.cs
+++ mc/Program.cs
@@ -32,10 +32,10 @@ namespace Minsk
                 }
 
                 var syntaxTree = SyntaxTree.Parse(line);
-                var binder = new Binder();
-                var boundExpression = binder.BindExpression(syntaxTree.Root);
+                var compilation = new Compilation(syntaxTree);
+                var result = compilation.Evaluate();
 
-                var diagnostics = syntaxTree.Diagnostics.Concat(binder.Diagnostics).ToArray();
+                var diagnostics = result.Diagnostics;
 
 
                 if (showTree)
@@ -47,9 +47,7 @@ namespace Minsk
 
                 if (!diagnostics.Any())
                 {
-                    var e = new Evaluator(boundExpression);
-                    var result = e.Evaluate();
-                    Console.WriteLine(result);
+                    Console.WriteLine(result.Value);
                 }
 
                 else

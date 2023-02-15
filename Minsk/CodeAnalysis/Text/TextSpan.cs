9a73134fbd36cc1ee02bf45c2c91d83b233b823a
Aadi Poddar <aadipoddarmail@gmail.com>
2023-02-16 01:15:42 +0530

    Add support for multiple lines in REPL

--- Minsk/CodeAnalysis/Text/TextSpan.cs
+++ Minsk/CodeAnalysis/Text/TextSpan.cs
@@ -12,7 +12,7 @@ namespace Minsk.CodeAnalysis.Text
         public int Length { get; }
         public int End => Start + Length;
 
-        internal static TextSpan FromBounds(int start, int end)
+        public static TextSpan FromBounds(int start, int end)
         {
             var length = end - start;
             return new TextSpan(start, length);
